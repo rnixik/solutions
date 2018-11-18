ba385e3609256633797e053650bab7018e411a64
rNix <rnix@yandex.ru>
2018-11-18 19:27:27 +0700

    Implement adder

---
+++ adder/Adder.php
@@ -0,0 +1,41 @@
+<?php
+
+class Adder
+{
+    /**
+     * @param string $number1
+     * @param string $number2
+     * @return string
+     */
+    public function add(string $number1, string $number2)
+    {
+        // Do column addition
+
+        // Pad them
+        $maxLength = max(strlen($number1), strlen($number2));
+        $number1 = str_pad($number1, $maxLength, '0', STR_PAD_LEFT);
+        $number2 = str_pad($number2, $maxLength, '0', STR_PAD_LEFT);
+
+        // Reverse them
+        $number1 = strrev($number1);
+        $number2 = strrev($number2);
+
+        $result = '';
+        $remain = 0;
+
+        for ($position = 0; $position < $maxLength; $position += 1) {
+            $digit1 = $number1[$position];
+            $digit2 = $number2[$position];
+            $sum = (int) $digit1 + (int) $digit2 + $remain;
+            $resultDigit = $sum % 10;
+            $remain = floor($sum / 10);
+            $result = ((string) $resultDigit) . $result;
+        }
+
+        if ($remain) {
+            $result = ((string) $remain) . $result;
+        }
+
+        return $result;
+    }
+}
---
+++ adder/test.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once 'Adder.php';
+$adder = new Adder();
+
+$testCases = [
+    ['123', '456', '579'],
+    ['123', '459', '582'],
+    ['1', '789', '790'],
+    ['654', '3', '657'],
+    ['45171761576165176517651765167875', '46765297916172279997849446441418', '91937059492337456515501211609293'],
+    ['0', '0', '0'],
+    ['999', '1', '1000'],
+    ['999', '0', '999'],
+    ['0', '999', '999'],
+    ['a', '999', '999'],
+    ['999', 'a', '999'],
+];
+
+$errorDetails = [];
+
+foreach ($testCases as $testCase) {
+    $sum = $adder->add($testCase[0], $testCase[1]);
+    if ($sum == $testCase[2]) {
+        echo '.';
+    } else {
+        echo 'E';
+        $errorDetails[] = "expected $testCase[2] != actual $sum  = $testCase[0] + $testCase[1]";
+    }
+}
+
+echo PHP_EOL;
+
+foreach ($errorDetails as $detail) {
+    echo $detail . PHP_EOL;
+}
+
+if (empty($errorDetails)) {
+    echo "Success" . PHP_EOL;
+} else {
+    echo 'Fail' . PHP_EOL;
+    exit(1);
+}
